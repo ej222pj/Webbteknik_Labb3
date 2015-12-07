5416653046a6b6f0bcc70f90c2cc7179493eaa5b
ej222pj <ej222pj@student.lnu.se>
2015-12-07 19:46:55 +0100

    Bytte till senaste versionen av maps api

--- script/mapFunctions.js
+++ script/mapFunctions.js
@@ -1,26 +1,16 @@
 "use strict";
 
-if (GBrowserIsCompatible())
-{
-	// create map component in div with the id = "map"
-	var map = new GMap2(document.getElementById("map"));
-	// create center point when map is displayed
-	map.setCenter(new GLatLng(37.4419, -122.1419), 13);
-	map.openInfoWindow(map.getCenter(),
-	"<b>Your Company Here</b>");
-	// re-open the info balloon if they close it
-	var point = new GLatLng(37.395746, -121.952234);
-	map.addOverlay(createMarker(point, 1));
-}
+var latlng = {lat: 57.469304, lng: 15.507875};
 
+// create map component in div with the id = "map"
+var map = new google.maps.Map(document.getElementById('map'), {
+	center: latlng,
+	zoom: 13
+});
 
-function createMarker(point, number)
+var marker = new google.maps.Marker(
 {
-	var marker = new GMarker(point);
-	// create clickable point with title for address
-	GEvent.addListener(marker, "click", function()
-	{
-		marker.openInfoWindowHtml("<b>Your Company Here</b>");
-	});
-	return marker;
-}
+	position: latlng,
+	map: map,
+	title: 'Pauli'
+});
